d9d4659fbaec9b4159a4274388cbf31b1d644b0f
Cui Huimin <anya25973@gmail.com>
2026-01-11 02:43:21 +0800

    Create ProductManagementServlet.java
    Product management method for Admins

---
+++ src/controller/ProductManagementServlet.java
@@ -0,0 +1,107 @@
+package controller;
+
+import dao.ProductDAO;
+import model.Product;
+import model.User;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.List;
+
+// Product management method for Admins
+@WebServlet("/ProductManagementServlet")
+public class ProductManagementServlet extends HttpServlet {
+
+    private ProductDAO productDAO = new ProductDAO();
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        // authority check
+        if (!isAdmin(request)) {
+            response.sendError(HttpServletResponse.SC_FORBIDDEN, "Access Denied.");
+            return;
+        }
+
+        // Fetch product list
+        List<Product> allProducts = productDAO.getAllProducts();
+        request.setAttribute("productList", allProducts);
+        request.getRequestDispatcher("admin_products.jsp").forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        if (!isAdmin(request)) {
+            response.sendError(HttpServletResponse.SC_FORBIDDEN, "Access Denied.");
+            return;
+        }
+
+        String action = request.getParameter("action");
+
+        if ("add".equals(action)) {
+            addProduct(request, response);
+        } else if ("update".equals(action)) {
+            updateProduct(request, response);
+        } else if ("delete".equals(action)) {
+            deleteProduct(request, response);
+        } else {
+            response.sendRedirect("ProductManagementServlet");
+        }
+    }
+
+    private void addProduct(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        Product p = new Product();
+        p.setName(request.getParameter("name"));
+        p.setDescription(request.getParameter("description"));
+        p.setPrice(Double.parseDouble(request.getParameter("price")));
+        p.setCategory(request.getParameter("category"));
+        p.setStock(Integer.parseInt(request.getParameter("stock")));
+        p.setImagePath(request.getParameter("imagePath"));
+
+        if (productDAO.addProduct(p)) {
+            response.sendRedirect("ProductManagementServlet?msg=add_success");
+        } else {
+            response.sendRedirect("ProductManagementServlet?msg=error");
+        }
+    }
+
+    private void updateProduct(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        Product p = new Product();
+        p.setId(Integer.parseInt(request.getParameter("id")));
+        p.setName(request.getParameter("name"));
+        p.setDescription(request.getParameter("description"));
+        p.setPrice(Double.parseDouble(request.getParameter("price")));
+        p.setCategory(request.getParameter("category"));
+        p.setStock(Integer.parseInt(request.getParameter("stock")));
+        p.setImagePath(request.getParameter("imagePath"));
+
+        if (productDAO.updateProduct(p)) {
+            response.sendRedirect("ProductManagementServlet?msg=update_success");
+        } else {
+            response.sendRedirect("ProductManagementServlet?msg=error");
+        }
+    }
+
+    private void deleteProduct(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        int id = Integer.parseInt(request.getParameter("id"));
+        if (productDAO.deleteProduct(id)) {
+            response.sendRedirect("ProductManagementServlet?msg=delete_success");
+        } else {
+            response.sendRedirect("ProductManagementServlet?msg=error");
+        }
+    }
+
+    private boolean isAdmin(HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+        User user = (session != null) ? (User) session.getAttribute("currentUser") : null;
+        return user != null && "0".equals(user.getRole());
+    }
+}
