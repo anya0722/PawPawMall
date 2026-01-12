a320ddc6e761ce88faa1c2558b089e12bae31b64
Cui Huimin <anya25973@gmail.com>
2026-01-13 04:35:33 +0800

    Change the timezone to malaysia
    Co-Authored-By: ZOU XINYAN <ekozou1206@student.usm.my>

--- src/util/DBUtil.java
+++ src/util/DBUtil.java
@@ -8,7 +8,7 @@ import java.sql.SQLException;
 
 public class DBUtil {
 
-    private static final String URL = "jdbc:mysql://localhost:3306/pawpawmall?useSSL=false&serverTimezone=UTC&allowPublicKeyRetrieval=true";
+    private static final String URL = "jdbc:mysql://localhost:3306/pawpawmall?serverTimezone=Asia/Kuala_Lumpur&useSSL=false";
     private static final String USER = "root";
     private static final String PASSWORD = "PawPawMall";
 
